60d8415c0bc4565b875d2a64f343283a88fa6c66
Chris McGravey <chris@mcgravey.dev>
2022-04-10 18:05:23 -0500

    testing

--- src/Controllers/tools.controller.ts
+++ src/Controllers/tools.controller.ts
@@ -1,27 +1,7 @@
-import { Body, Controller, Header, Headers, Post, Req } from '@nestjs/common';
+import { Controller, Headers, Post, Req } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { createHmac, timingSafeEqual } from 'crypto';
-import { Request } from 'express';
-
-import {
-  createParamDecorator,
-  ExecutionContext,
-  BadRequestException,
-} from '@nestjs/common';
-
 import * as rawBody from 'raw-body';
-
-export const PlainBody = createParamDecorator(
-  async (_, context: ExecutionContext) => {
-    const req = context.switchToHttp().getRequest<import('express').Request>();
-    if (!req.readable) {
-      throw new BadRequestException('Invalid body');
-    }
-
-    const body = (await rawBody(req)).toString('utf8').trim();
-    return body;
-  },
-);
 
 @Controller('tools')
 export class ToolsController {
@@ -29,11 +9,13 @@ export class ToolsController {
   @Post('deploy')
   async create(
     @Headers('X-Hub-Signature-256') webhookSecret: string,
-    @PlainBody() body,
+    @Req() req,
   ) {
     console.log('testing');
     const sig = Buffer.from(webhookSecret, 'utf8');
     const hmac = createHmac('sha256', this.configService.get('deploy').secret);
+
+    const body = (await rawBody(req)).toString('utf8').trim();
 
     const digest = Buffer.from(
       'sha256=' + hmac.update(body).digest('hex'),
