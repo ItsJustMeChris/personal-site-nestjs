cd27f4f11b20279f9866bbafc11a843a5e03300c
Chris McGravey <chris@mcgravey.dev>
2022-04-10 13:15:29 -0500

    Updates

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './Modules/app.module';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'http://localhost:4200',
-    optionsSuccessStatus: 200,
-  });
-  await app.listen(process.env.PORT || 3000);
+  if (process.env.SSLKEY && process.env.SSLCERT && process.env.SSLCHAIN) {
+    const httpsOptions = {
+      key: fs.readFileSync(process.env.SSLKEY),
+      cert: fs.readFileSync(process.env.SSLCERT),
+      chain: fs.readFileSync(process.env.SSLCHAIN),
+    };
+    const app = await NestFactory.create(AppModule, {
+      httpsOptions,
+    });
+    app.enableCors({
+      origin: process.env.CORSORIGIN,
+      optionsSuccessStatus: 200,
+    });
+    await app.listen(process.env.PORT || 3000);
+  } else {
+    const app = await NestFactory.create(AppModule);
+    app.enableCors({
+      origin: process.env.CORSORIGIN,
+      optionsSuccessStatus: 200,
+    });
+    await app.listen(process.env.PORT || 3000);
+  }
 }
 bootstrap();
