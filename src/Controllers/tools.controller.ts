a66c95668b04a3aa6074449e611f1b725465c6af
Chris McGravey <chris@mcgravey.dev>
2022-04-10 17:51:57 -0500

    debug body

--- src/Controllers/tools.controller.ts
+++ src/Controllers/tools.controller.ts
@@ -5,7 +5,8 @@ import { ConfigService } from '@nestjs/config';
 export class ToolsController {
   constructor(private configService: ConfigService) {}
   @Post('deploy')
-  async create(@Body('secret') secret: string) {
+  async create(@Body('secret') secret: string, @Body() body) {
+    console.log(body);
     if (secret === this.configService.get('deploy').secret) {
       return {
         success: true,
