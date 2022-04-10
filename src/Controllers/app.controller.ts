6d87f8d3880640465d38a1ac9db74c4ed4ebc8a4
Chris McGravey <chris@mcgravey.dev>
2022-04-10 18:45:39 -0500

    test

--- src/Controllers/app.controller.ts
+++ src/Controllers/app.controller.ts
@@ -3,9 +3,10 @@ import { Controller, Get } from '@nestjs/common';
 @Controller()
 export class AppController {
   @Get()
-  index(): { author: string } {
+  index(): { author: string; test: string } {
     return {
       author: 'ItsJustMeChris',
+      test: 'ing',
     };
   }
 }
