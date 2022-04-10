2b762eb351cfbdd7866fdd897fb179405137f5a0
Chris McGravey <chris@mcgravey.dev>
2022-04-10 18:15:17 -0500

    Testing

--- src/Controllers/tools.controller.ts
+++ src/Controllers/tools.controller.ts
@@ -1,34 +1,12 @@
-import { Controller, Headers, Post, Req } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { createHmac, timingSafeEqual } from 'crypto';
-import * as rawBody from 'raw-body';
+import { Controller, Post, UseGuards } from '@nestjs/common';
+import { DeployGuard } from 'src/Guards/deploy.guard';
 
 @Controller('tools')
 export class ToolsController {
-  constructor(private configService: ConfigService) {}
+  @UseGuards(DeployGuard)
   @Post('deploy')
-  async create(
-    @Headers('X-Hub-Signature-256') webhookSecret: string,
-    @Req() req,
-  ) {
+  async create() {
     console.log('testing');
-    const sig = Buffer.from(webhookSecret, 'utf8');
-    const hmac = createHmac('sha256', this.configService.get('deploy').secret);
-
-    const body = (await rawBody(req)).toString('utf8').trim();
-
-    const digest = Buffer.from(
-      'sha256=' + hmac.update(body).digest('hex'),
-      'utf8',
-    );
-    if (sig.length !== digest.length || !timingSafeEqual(digest, sig)) {
-      console.log('not safe');
-      return {
-        success: false,
-      };
-    }
-
-    console.log('safe');
 
     return {
       success: true,
---
+++ src/Guards/deploy.guard.ts
@@ -0,0 +1,30 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as crypto from 'crypto';
+
+@Injectable()
+export class DeployGuard implements CanActivate {
+  constructor(private configService: ConfigService) {}
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const secretKey = this.configService.get('deploy').secret;
+
+    const hmac = request.headers['X-Hub-Signature-256'];
+
+    const hash = crypto
+      .createHmac('sha256', secretKey)
+      .update(request.body)
+      .digest('base64');
+
+    if (hmac === hash) {
+      return true;
+    } else {
+      throw new ForbiddenException('Not allowed');
+    }
+  }
+}
