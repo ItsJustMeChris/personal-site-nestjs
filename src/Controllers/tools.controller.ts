47c8e8355d73a2e6ebcdee7aadd1cc6049b52aa7
Chris McGravey <chris@mcgravey.dev>
2022-04-10 19:26:23 -0500

    changes

--- src/Controllers/tools.controller.ts
+++ src/Controllers/tools.controller.ts
@@ -1,14 +1,20 @@
 import { Controller, Post, UseGuards } from '@nestjs/common';
 import { DeployGuard } from 'src/Guards/deploy.guard';
 import * as shell from 'shelljs';
+import npm from 'npm';
 
 @Controller('tools')
 export class ToolsController {
   @UseGuards(DeployGuard)
   @Post('deploy')
   deploy() {
-    shell.exec(
-      'cd ~/personal-site-nestjs && git pull && npm run build && systemctl restart api',
+    shell.exec('cd ~/personal-site-nestjs && git pull');
+    npm.commands.exec(
+      ['build'],
+      (err: Error, result: any, result2: any, result3: any, result4: any) => {
+        console.log(err, result, result2, result3, result4);
+        shell.exec('systemctl restart api');
+      },
     );
     return {
       success: true,
