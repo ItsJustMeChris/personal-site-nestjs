8d435e680ae10dc3374175d94cf38424156c7932
Chris McGravey <chris@mcgravey.dev>
2022-04-10 19:30:47 -0500

    test

--- src/Controllers/tools.controller.ts
+++ src/Controllers/tools.controller.ts
@@ -8,14 +8,7 @@ export class ToolsController {
   @UseGuards(DeployGuard)
   @Post('deploy')
   deploy() {
-    shell.exec('cd ~/personal-site-nestjs && git pull');
-    npm.commands.exec(
-      ['build'],
-      (err: Error, result: any, result2: any, result3: any, result4: any) => {
-        console.log(err, result, result2, result3, result4);
-        shell.exec('systemctl restart api');
-      },
-    );
+    shell.exec('npm run deploy');
     return {
       success: true,
     };
