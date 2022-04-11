798e93f2c7a2225d8156722aae1a2e33c010f0d9
Chris McGravey <chris@mcgravey.dev>
2022-04-10 19:17:57 -0500

    execute

--- src/Controllers/tools.controller.ts
+++ src/Controllers/tools.controller.ts
@@ -1,17 +1,15 @@
 import { Controller, Post, UseGuards } from '@nestjs/common';
-import { exec, spawn } from 'child_process';
 import { DeployGuard } from 'src/Guards/deploy.guard';
+import * as shell from 'shelljs';
 
 @Controller('tools')
 export class ToolsController {
   @UseGuards(DeployGuard)
   @Post('deploy')
   deploy() {
-    spawn(
+    shell.exec(
       'cd ~/personal-site-nestjs && git pull && npm run build && systemctl restart api',
-      {},
     );
-
     return {
       success: true,
     };
