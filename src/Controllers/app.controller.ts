e3e89e4c67a679b80b10103314a7948fc6c43651
Chris McGravey <chris@mcgravey.dev>
2022-04-11 18:33:51 -0500

    Update api index

--- src/Controllers/app.controller.ts
+++ src/Controllers/app.controller.ts
@@ -12,9 +12,20 @@ export class AppController {
       nodejs: process.version,
       ackolwedgements: {
         nestjs: 'https://nestjs.com/',
+        nodejs: 'https://nodejs.org/',
         trakttv: 'https://trakt.tv/',
-        lastfm: 'https://www.last.fm/',
+        lastfm: 'https://last.fm/',
       },
+      donate: {
+        'Slava Ukraini':
+          'https://reddit.com/r/ukraine/comments/s6g5un/want_to_support_ukraine_heres_a_list_of_charities/',
+        'heart.org': 'https://heart.org/donate/',
+        'nokidhungry.org': 'https://nokidhungry.org/',
+        save_the_earth:
+          'https://www.effectiveenvironmentalism.org/climate-charities',
+        crypto: 'https://thegivingblock.com/',
+      },
+      stop: 'war',
     };
   }
 }
