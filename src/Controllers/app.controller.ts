2593d0db1202eb599ac72781eeac8bb869dd2878
Chris McGravey <chris@mcgravey.dev>
2022-04-10 19:55:03 -0500

    changes

--- src/Controllers/app.controller.ts
+++ src/Controllers/app.controller.ts
@@ -3,9 +3,12 @@ import { Controller, Get } from '@nestjs/common';
 @Controller()
 export class AppController {
   @Get()
-  index(): { author: string } {
+  index() {
     return {
       author: 'ItsJustMeChris',
+      repository: 'https://github.com/ItsJustMeChris/personal-site-nestjs',
+      version: process.env.npm_package_version,
+      nodejs: process.version,
     };
   }
 }
